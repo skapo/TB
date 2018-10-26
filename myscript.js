cf4ad94981dd6e21c805204e6dcb4bf777c305e5
skapo <gamers0603@gmail.com>
2018-10-26 11:30:48 +0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -4,8 +4,8 @@
 // @version      1.0.3
 // @description  BitTube Never Stop Ever Again
 // @author       ReaperBot
-// @updateURL    https://raw.githubusercontent.com/skapo/js/master/BT.js
-// @downloadURL  https://raw.githubusercontent.com/skapo/js/master/BT.js
+// @updateURL    https://github.com/skapo/TB/blob/master/myscript.js
+// @downloadURL  https://github.com/skapo/TB/blob/master/myscript.js
 // @match        *.bit.tube/*
 // @grant        none
 // ==/UserScript==
