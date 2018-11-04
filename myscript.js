cb9dccd65b66d14267cda20cb1eedd0a9732fffd
skapo <gamers0603@gmail.com>
2018-11-04 08:23:45 +0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -18,7 +18,7 @@
     
 
     var fileme = [
-        'https://bit.tube/play?hash=BRKWEjIx6FQfUdDIQPPa&channel=37042',
+        'https://bit.tube/play?hash=BRp5tEwigij9zc8DAEhw&channel=37042',
         'https://bit.tube/play?hash=BRenwDPKEriE4qpvYJ7P&channel=38479',
     ];
     
