0eb66638090547b9054a9528c0332c04dee93384
skapo <gamers0603@gmail.com>
2018-11-05 23:25:28 +0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -18,8 +18,8 @@
     
 
     var fileme = [
-        'https://bit.tube/play?hash=BRenwDPKEriE4qpvYJ7P&channel=38479',
         'https://bit.tube/play?hash=BR9IUE5FMSXXh9rOIDM2&channel=36400',
+        'https://bit.tube/play?hash=BRenwDPKEriE4qpvYJ7P&channel=38479',
     ];
     
     setInterval(mySearch, 5000);
