4dcbf6b44934535b94eee6a732cfca17b4dfd379
skapo <gamers0603@gmail.com>
2018-11-05 09:43:11 +0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -46,7 +46,7 @@
         return (Math.floor(Math.random() * (max - min)) + min) * 1000;
     }
 
-    setInterval(reloadOk, getRndInteger(3600, 6300));
+    setInterval(reloadOk, getRndInteger(5760, 7200));
 
     function reloadOk() {
         window.location.href = random_item(fileme)
